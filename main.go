83ee5d3e2c21496aa118a0d0a7d074d79c6298f2
Bertie Blackman <blackmanrgh@gmail.com>
2018-11-01 14:39:03 +0000

    Refactors commands into seperate package

--- main.go
+++ main.go
@@ -4,90 +4,16 @@ import (
 	"log"
 	"os"
 
-	"github.com/covertbert/iex-cli/stock"
+	"github.com/covertbert/iex-cli/commands"
 	"github.com/urfave/cli"
 )
-
-var cliCommands = []cli.Command{
-	{
-		Name:    "book",
-		Aliases: []string{"b"},
-		Usage:   "view a symbol's book - Requires subcommand and symbol e.g. iex-cli book quote AAPL",
-		Subcommands: []cli.Command{
-			{
-				Name:    "quote",
-				Aliases: []string{"q"},
-				Usage:   "view quote",
-				Action: func(c *cli.Context) error {
-					stock.QueryBook(c.Args().First(), "quote")
-					return nil
-				},
-			},
-			{
-				Name:    "bids",
-				Aliases: []string{"b"},
-				Usage:   "view bids",
-				Action: func(c *cli.Context) error {
-					stock.QueryBook(c.Args().First(), "bids")
-					return nil
-				},
-			},
-			{
-				Name:    "asks",
-				Aliases: []string{"a"},
-				Usage:   "view asks",
-				Action: func(c *cli.Context) error {
-					stock.QueryBook(c.Args().First(), "asks")
-					return nil
-				},
-			},
-			{
-				Name:    "trades",
-				Aliases: []string{"t"},
-				Usage:   "view trades",
-				Action: func(c *cli.Context) error {
-					stock.QueryBook(c.Args().First(), "trades")
-					return nil
-				},
-			},
-		},
-	},
-	{
-		Name:    "company",
-		Aliases: []string{"c"},
-		Usage:   "view a symbol's general information - e.g. iex-cli company AAPL",
-		Action: func(c *cli.Context) error {
-			stock.QueryCompany(c.Args().First())
-			return nil
-		},
-	},
-	{
-		Name:    "crypto",
-		Aliases: []string{"cr"},
-		Usage:   "view a symbol's general information - e.g. iex-cli company AAPL",
-		Action: func(c *cli.Context) error {
-			stock.QueryCrypto()
-			return nil
-		},
-	},
-	{
-		Name:    "ohlc",
-		Aliases: []string{"ohlc"},
-		Usage:   "view a symbol's official open and close - e.g. iex-cli ohlc AAPL",
-		Action: func(c *cli.Context) error {
-			stock.QueryOHLC(c.Args().First())
-			return nil
-		},
-	},
-}
 
 func main() {
 	app := cli.NewApp()
 
 	app.Name = "iex-cli"
 	app.Usage = "Stock market information in the command line via iex"
-
-	app.Commands = cliCommands
+	app.Commands = commands.CliCommands
 
 	err := app.Run(os.Args)
 
